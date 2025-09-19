efe058258cff33f52ad953e92b447401e73651f1
Igor <89994712+ownerigor@users.noreply.github.com>
2025-09-18 23:21:05 -0300

    Adding new flags for password creation

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	password := GeneratePassword(16)
-	fmt.Printf("Generated secure password: %s", password)
+	length := flag.Int("length", 16, "Length of the password")
+	useNumbers := flag.Bool("numbers", true, "Include numbers in the password")
+	useSymbols := flag.Bool("symbols", true, "Include symbols in the password")
+	count := flag.Int("count", 1, "Number of passwords to generate")
+
+	flag.Parse()
+
+	for i := 0; i < *count; i++ {
+		password := GeneratePassword(*length, *useNumbers, *useSymbols)
+		fmt.Println(password)
+	}
 }
--- password.go
+++ password.go
@@ -5,18 +5,34 @@ import (
 	"math/big"
 )
 
-var charset = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?")
+var (
+	lowercase = []rune("abcdefghijklmnopqrstuvwxyz")
+	uppercase = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	numbers   = []rune("0123456789")
+	symbols   = []rune("!@#$%^&*()_+-=[]{}|;:,.<>?")
+)
 
 func randomInt(max int64) int64 {
 	n, _ := rand.Int(rand.Reader, big.NewInt(max))
 	return n.Int64()
 }
 
-func GeneratePassword(length int) string {
-	var password []rune
+func GeneratePassword(length int, useNumbers bool, useSymbols bool) string {
+	charset := append([]rune{}, lowercase...)
+	charset = append(charset, uppercase...)
+
+	if useNumbers {
+		charset = append(charset, numbers...)
+	}
+
+	if useSymbols {
+		charset = append(charset, symbols...)
+	}
+
+	password := make([]rune, length)
 	for i := 0; i < length; i++ {
 		idx := randomInt(int64(len(charset)))
-		password = append(password, charset[idx])
+		password[i] = charset[idx]
 	}
 	return string(password)
 }
